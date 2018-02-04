3dbd3a05b406e768a93bfb126f6a8f1d029643e4
Mohamed Dahman <mhd.dahman@yahoo.com>
2018-02-04 08:20:56 +0000

    version 5.0.1

--- application.py
+++ application.py
@@ -10,6 +10,8 @@ import cgi, cgitb
 from helpers import login_required,mealProcess
 from helpers import isUserExsist , getUserHash ,isPasswordFormated , changeUserHash
 from helpers import addUser
+import os
+
 
 
 # Configure application
@@ -37,25 +39,29 @@ Session(app)
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///kitchen.db")
 
+UPLOAD_FOLDER = 'static'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/shopping", methods=["GET", "POST"])
+@login_required
 def shopping():
     """shopping"""
     if request.method == "POST":
         if not request.form.get("price"):
             return apology("must provide Total-price", 403)
-
         Total = request.form.get("price")
-        Total_Paid = db.execute("INSERT INTO cost (amount) VALUES(:amount)",
-                          Total=request.form.get("price"))
-
+        S1 = request.form.get("s1")
+        print(S1)
+        db.execute("INSERT INTO cost (amount,mealId,userId) VALUEs(:X,:Y,:Z)",
+                          X=Total, Y=S1 ,Z=session["user_id"])
         return redirect("/")
     else:
          mealsRow = db.execute("""SELECT * FROM mealProcess, meals WHERE
-                                mealProcess.mealId = meals.id AND mealProcess.userId = :userid AND mealProcess.community = 4""",
-                                userid = session["user_id"]    )
-
+                                mealProcess.mealId = meals.id AND mealProcess.userId = :userid AND mealProcess.community = 4
+                                and meals.id not in (select mealid from cost)""",
+                                userid = session["user_id"])
          return render_template("shopping.html", meals = mealsRow)
+
 
 
 @app.route("/")
@@ -72,6 +78,7 @@ def index():
         currentRow = 0
         while currentRow <= len(rows) - 1:
             rowData = {}
+            rowData['id'] = rows[currentRow]["id"]
             rowData['name'] = rows[currentRow]["name"]
             rowData['date'] = rows[currentRow]["date"]
 
@@ -107,11 +114,44 @@ def index():
                     rowData['Cleaner'] =0
 
 
+
+            Participate = db.execute("select count(*) as Counts,userId as helper from  mealProcess where community =5 and mealId = :mealId group by userId",
+                               mealId=rows[currentRow]["id"])
+            if len(Participate) == 1:
+                    users = db.execute("select userName from  users where id = :userid",
+                                userid=Participate[0]["helper"])
+                    rowData['Participate'] = users[0]["username"]
+            else:
+                    rowData['Participate'] =0
+
+
+            ParticipateCount = db.execute("select count(*) as Counts from  mealProcess where community =5 and mealId = :mealId ",
+                               mealId=rows[currentRow]["id"])
+            count =  ParticipateCount[0]["Counts"]
+
+            workerCount = db.execute("select count(distinct userId) as userCount from  mealProcess where  mealId = :mealId",
+                               mealId=rows[currentRow]["id"])
+            count +=  workerCount[0]["userCount"]
+
+            rowData['ParticipateCount'] = count
+
             listRowData.append(rowData)
             currentRow = currentRow + 1
 
+
+
         return render_template("index.html", meals = listRowData)
 
+
+
+@app.route("/addParticipate", methods=["GET", "POST"])
+@login_required
+def addParticipate():
+    mealId = request.form.get("addParticipate")
+    currentUserId = session["user_id"]
+    community = 5 # 	Cooker Helper
+    mealProcess(mealId, community, currentUserId)
+    return redirect("/")
 
 
 @app.route("/addHellper", methods=["GET", "POST"])
@@ -122,7 +162,6 @@ def addCokker():
     community = 2 # 	Cooker Helper
     mealProcess(mealId, community, currentUserId)
     return redirect("/")
-
 
 
 @app.route("/addShopper", methods=["GET", "POST"])
@@ -206,7 +245,7 @@ def login():
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
-
+        #app.jinja_env.globals.update(current_username = "Hi "+ request.form.get("username"))
         # Redirect user to home page
         return redirect("/")
 
@@ -264,44 +303,95 @@ def register():
         return render_template("register.html")
 
 
+
 @app.route("/addsug", methods=["GET", "POST"])
 @login_required
 def addsug():
 
-    if request.method == "POST":
-        mealName = request.form.get("name")
-        mealDes = request.form.get("description")
-        mealDate = request.form.get("date")
-
-        suggestion = db.execute("INSERT INTO meals (name, description, date, userId) VALUES (:mName, :mDescription, :mDate ,:user_id)",
-                            mName=mealName, mDescription=mealDes, mDate=mealDate , user_id=session["user_id"]  )
+   if request.method == "POST":
+       mealName = request.form.get("name")
+       mealDes = request.form.get("description")
+       mealDate = request.form.get("date")
 
 
+       file = request.files['image']
+       f = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
 
-        return redirect("/")
-    else:
-        return render_template("addsug.html")
+       # add your custom code to check that the uploaded file is a valid image and not a malicious file (out-of-scope for this post)
+       file.save(f)
+
+
+       suggestion = db.execute("INSERT INTO meals (name, description, date, userId) VALUES (:mName, :mDescription, :mDate ,:user_id)",
+                           mName=mealName, mDescription=mealDes, mDate=mealDate , user_id=session["user_id"]  )
+       Maxidrows = db.execute("SELECT max(id) as id FROM meals ")
+       Maxid = Maxidrows[0]["id"]
+       newfilename = UPLOAD_FOLDER+"/"+str(Maxid) +".jpg"
+       os.rename(f,newfilename)
+       unitsrows = db.execute("SELECT description FROM units")
+       return render_template("materialdetails.html",units =unitsrows ,mealid=Maxid, mealName=mealName,mealDes=mealDes,mealDate=mealDate,cook=session["user_id"])
+   else:
+       return render_template("addsug.html")
+
+
+
+
+
+@app.route("/delrecord", methods=["GET", "POST"])
+@login_required
+def delrecord():
+
+   if request.method == "POST":
+       mealid = request.form.get("delrecord")
+
+       materials = db.execute("delete  from mealsDetails  where mealid = :mealid ", mealid = mealid )
+
+       redirect("/")
+   else:
+
+       return render_template("materialdetails.html" )
+
+
+
 
 @app.route("/addrecord", methods=["GET", "POST"])
 @login_required
 def addrecord():
 
-    if request.method == "POST":
-        mealName = request.form.get("material")
-        mealDes = request.form.get("quantity")
-        mealDate = request.form.get("unit")
+   if request.method == "POST":
+       material = request.form.get("name")
+       quantity = request.form.get("quantity")
+       unit = request.form.get("unit")
+       meal = request.form.get("mealid")
 
 
-        suggestion = db.execute("INSERT INTO meals (name, description, date, userId) VALUES (:mName, :mDescription, :mDate ,:user_id)",
-                            mName=mealName, mDescription=mealDes, mDate=mealDate , user_id=session["user_id"]  )
-        username = db.execute("SELECT username FROM users WHERE id=:user_id;",
-                             user_id=session["user_id"])
+       materials = db.execute("INSERT INTO mealsDetails (material, mealId ,quntity, unit) VALUES (:material,:mealid, :quantity, :unit)",
+                              mealid = meal ,material=material, quantity=quantity, unit=unit)
+
+       mealsrows = db.execute("SELECT * FROM meals WHERE id=:mealid",mealid = meal )
+
+       materialsrows = db.execute("SELECT * FROM mealsDetails WHERE mealId=:mealid",mealid = meal )
+       print(len(materialsrows))
+      #
+       materialsrows_options = []
+       for i in range(len(materialsrows)):
+           new_materialsrows = {}
+           new_materialsrows['material'] = materialsrows[i]['material']
+           new_materialsrows['quntity'] = materialsrows[i]['quntity']
+           new_materialsrows['unit'] = materialsrows[i]['unit']
+           materialsrows_options.append(new_materialsrows)
+      #
+
+       unitsrows = db.execute("SELECT description FROM units")
+
+       return render_template("materialdetails.html",units =unitsrows , materials=materialsrows_options,mealid=mealsrows[0]["id"], mealName=mealsrows[0]["name"],mealDes=mealsrows[0]["description"],mealDate=mealsrows[0]["date"],cook=session["user_id"])
 
 
+       #return render_template("materialdetails.html",materials=materialsrows)
 
-        return render_template("materialdetails.html",mealName=mealName,mealDes=mealDes,mealDate=mealDate,cook=username[0]['username'])
-    else:
-        return render_template("materialdetails.html")
+   else:
+
+       return render_template("materialdetails.html" )
+
 
 
 @app.route("/units", methods=["GET", "POST"])
@@ -322,12 +412,83 @@ def units():
         db.execute("INSERT INTO units (description) VALUES (:description)",
                    description=units)
 
+        # Query the user units
+        units = db.execute("SELECT * FROM units")
+
         # Render template and pass data to quoted result page
-        return render_template("/units.html")
+        return render_template("units.html", units=units)
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
-        return render_template("units.html")
+        units = db.execute("SELECT * FROM units")
+
+        # Render template and pass data to quoted result page
+        return render_template("units.html", units=units)
+
+
+@app.route("/editUnit", methods=["GET", "POST"])
+@login_required
+def editUnit():
+    """Get stock quote."""
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        units = request.form.get("description_details")
+        unitid = request.form.get("editUnit")
+
+        # Ensure units were entered
+        if not editUnit:
+            return apology("must provide valid units", 400)
+
+        # Insert the units into units table
+        db.execute("UPDATE units SET description = :description WHERE id = :unitid",
+                   description=units, unitid=unitid)
+
+        # Query the user units
+        units = db.execute("SELECT * FROM units")
+
+        # Render template and pass data to quoted result page
+        return render_template("units.html", units=units)
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        units = db.execute("SELECT * FROM units")
+
+        # Render template and pass data to quoted result page
+        return render_template("units.html", units=units)
+
+
+@app.route("/deleteUnit", methods=["GET", "POST"])
+@login_required
+def deleteUnit():
+    """Get stock quote."""
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        unitid = request.form.get("deleteUnit")
+
+        # Insert the units into units table
+        db.execute("DELETE FROM units WHERE id = :unitid",
+                   unitid=unitid)
+
+        # Query the user units
+        units = db.execute("SELECT * FROM units")
+
+        # Render template and pass data to quoted result page
+        return render_template("units.html", units=units)
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        units = db.execute("SELECT * FROM units")
+
+        # Render template and pass data to quoted result page
+        return render_template("units.html", units=units)
+
+
+
+
 
 def errorhandler(e):
     """Handle error"""
